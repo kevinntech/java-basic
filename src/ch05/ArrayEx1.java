e30250c918c997c22fb1c584c5bafb7d85f008e6
kevinntech <kevinntech@nate.com>
2020-05-18 18:21:42 +0900

    자바의 정석 - 배열 1

---
+++ src/ch05/ArrayEx1.java
@@ -0,0 +1,24 @@
+package ch05;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+        int[] score = new int[5];
+        int k = 1;
+
+        score[0] = 50;
+        score[1] = 60;
+        score[k+1] = 70;
+        score[3] = 80;
+        score[4] = 90;
+
+        int tmp = score[k+2] + score[4];    // 170
+
+        for(int i = 0; i < 5; i++){
+            System.out.printf("score[%d]:%d%n", i, score[i]);
+        }
+
+        System.out.printf("tmp:%d%n", tmp); // 170
+        //System.out.printf("score[%d]:%d%n", 7, score[7]);   // 실행 , 에러 발생
+
+    }
+}

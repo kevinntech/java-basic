472254dcc9f17a26e856dfb4195262daf08ad984
kevin <kevinntech@nate.com>
2020-08-01 22:53:40 +0900

    Ch07 복습

--- src/ch07/PolyArgumentTest.java
+++ src/ch07/PolyArgumentTest.java
@@ -1,22 +1,22 @@
 package ch07;
 
 class Product {
-	int price;
-	int bonusPoint;
+	int price;			// 제품의 가격
+	int bonusPoint;		// 제품구매 시 제공하는 보너스점수
 
 	Product(int price) {
 		this.price = price;
-		bonusPoint =(int)(price/10.0);
+		bonusPoint =(int)(price/10.0);	// 보너스점수는 제품가격의 10%
 	}
 }
 
 class Tv extends Product {
 	Tv() {
-
-		super(100);
+		// 조상클래스의 생성자 Product(int price)를 호출한다.
+		super(100);			// Tv의 가격을 100만원으로 한다.
 	}
 
-	public String toString() {	// ObjectŬ������ toString()�� �������̵��Ѵ�.
+	public String toString() {	// Object클래스의 toString()을 오버라이딩한다.
 		return "Tv";
 	}
 }
@@ -31,19 +31,19 @@ class Computer extends Product {
 	}
 }
 
-class Buyer {
-	int money = 1000;
-	int bonusPoint = 0;
+class Buyer {			// 고객, 물건을 사는 사람
+	int money = 1000;	// 소유금액
+	int bonusPoint = 0;	// 보너스점수
 
 	void buy(Product p) {
 		if(money < p.price) {
-			System.out.println("�ܾ��� �����Ͽ� ������ ��� �����ϴ�.");
+			System.out.println("잔액이 부족하여 물건을 살수 없습니다.");
 			return;
 		}
 
-		money -= p.price;
-		bonusPoint += p.bonusPoint;
-		System.out.println(p + "��/�� �����ϼ̽��ϴ�.");
+		money -= p.price;			// 가진 돈에서 구입한 제품의 가격을 뺀다.
+		bonusPoint += p.bonusPoint;	// 제품의 보너스 점수를 추가한다.
+		System.out.println(p + "을/를 구입하셨습니다.");
 	}
 }
 
@@ -54,7 +54,7 @@ class PolyArgumentTest {
 		b.buy(new Tv());
 		b.buy(new Computer());
 
-		System.out.println("���� ���� ���� " + b.money + "�����Դϴ�.");
-		System.out.println("���� ���ʽ������� " + b.bonusPoint + "���Դϴ�.");
+		System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
+		System.out.println("현재 보너스점수는 " + b.bonusPoint + "점입니다.");
 	}
 }
--- src/ch07/TimeTest.java
+++ src/ch07/TimeTest.java
@@ -22,17 +22,23 @@ class Time {
             setSecond(second); 
       } 
 
-      public int getHour() { return hour; } 
+      public int getHour() {
+            return hour;
+      }
       public void setHour(int hour) { 
             if (hour < 0 || hour > 23) return; 
             this.hour = hour; 
       } 
-      public int getMinute() { return minute; } 
+      public int getMinute() {
+            return minute;
+      }
       public void setMinute(int minute) { 
             if (minute < 0 || minute > 59) return; 
             this.minute = minute; 
       } 
-      public int getSecond() { return second; } 
+      public int getSecond() {
+            return second;
+      }
       public void setSecond(int second) { 
             if (second < 0 || second > 59) return; 
             this.second = second; 
